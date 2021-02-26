fb7b410c101aef190061a34b0e19ffc3214959d1
Garrett <gman9mm@live.com>
2021-02-26 15:39:14 -0800

    It's rainin' boxes! (Halleleujah.)

--- MatterExploration/MatterJsExp1/sketch.js
+++ MatterExploration/MatterJsExp1/sketch.js
@@ -5,18 +5,23 @@ var Engine = Matter.Engine,
 
 var engine;
 var world;
-var box1;
+var boxes = [];
 
 function setup() {
   createCanvas(400, 400);
   engine = Engine.create();
   world = engine.world;
   Engine.run(engine);
-  box1 = new Box(200, 100, 50, 50);
 
+}
+
+function mousePressed() {
+  boxes.push(new Box(mouseX, mouseY, 20, 20));
 }
 
 function draw() {
   background(51);
-  box1.show();
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+  }
 }
