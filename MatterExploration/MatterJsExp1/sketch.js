4bb9b65679b933ace318757fd16ace022d4ab769
Garrett <gman9mm@live.com>
2021-02-26 15:16:44 -0800

    Add box to world, now drops.

--- MatterExploration/MatterJsExp1/sketch.js
+++ MatterExploration/MatterJsExp1/sketch.js
@@ -11,12 +11,13 @@ function setup() {
   createCanvas(400, 400);
   engine = Engine.create();
   world = engine.world;
-  box1 = Bodies.rectangle(400, 200, 80, 80);
+  box1 = Bodies.rectangle(200, 100, 80, 80);
   Engine.run(engine);
-  console.log(box1);
+  World.add(world, box1);
 }
 
 function draw() {
   background(51);
 
+  rect(box1.position.x, box1.position.y, 80, 80)
 }
