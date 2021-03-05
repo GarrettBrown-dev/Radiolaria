ba2aa793db16df6a90bf1eccc7650d3b7c15434a
Garrett <gman9mm@live.com>
2021-03-05 12:11:36 -0800

    Doing extra work during lunch before flight. Add more particles.

--- MatterExploration/MatterjsExp2/sketch.js
+++ MatterExploration/MatterjsExp2/sketch.js
@@ -15,23 +15,23 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-  for (var x = 20; x < 380; x += 20) {
+  for (var x = 20; x < 380; x += 40) {
 
-    var p1 = new Particle(230, 100, 10);
-    var p2 = new Particle(200, 150, 10);
-    // particles.push(p1, p2);
+    var p1 = new Particle(x, 100, 10);
+    // var p2 = new Particle(200, 150, 10);
+    particles.push(p1);
 
     // var options = {
     //   bodyA: p1.body,
     //   bodyB: p2.body,
     //   length: 50,
     //   stiffness: 0.4
-  }
-  // var constraint = Constraint.create(options);
+    // var constraint = Constraint.create(options);
 
-  // World.add(world, constraint);
+    // World.add(world, constraint);
+  }
+  boundaries.push(new Boundary(200, height, width, 100));
 }
-boundaries.push(new Boundary(200, height, width, 100));
 
 
 function draw() {
@@ -44,6 +44,6 @@ function draw() {
     particles[i].show();
   }
 
-  line(particles[0].body.position.x, particles[0].body.position.y, particles[1].body.position.x, particles[1].body.position.y);
+  // line(particles[0].body.position.x, particles[0].body.position.y, particles[1].body.position.x, particles[1].body.position.y);
 
 }
