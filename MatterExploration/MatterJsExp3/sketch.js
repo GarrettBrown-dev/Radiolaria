128ac7d27e6734e746fcdace12058f7c3a74829d
Garrett <gman9mm@live.com>
2021-03-08 10:56:44 -0800

    Ground is now static.

--- MatterExploration/MatterJsExp3/sketch.js
+++ MatterExploration/MatterJsExp3/sketch.js
@@ -7,7 +7,7 @@ function setup() {
   createCanvas(600, 400);
   engine = Matter.Engine.create();
   world = engine.world;
-  ground = new Box(width / 2, height - 10, width, 20);
+  ground = new Ground(width / 2, height - 10, width, 20);
   box = new Box(450, 300, 50, 75);
   bird = new Bird(50, 300, 25);
 }
