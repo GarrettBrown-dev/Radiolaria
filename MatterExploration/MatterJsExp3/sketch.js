c7d13e4088d813f18789b9ef7b1f4c082d265731
Garrett <gman9mm@live.com>
2021-03-08 11:53:27 -0800

    Constraint releases on spacebar.

--- MatterExploration/MatterJsExp3/sketch.js
+++ MatterExploration/MatterJsExp3/sketch.js
@@ -15,9 +15,9 @@ function setup() {
   for (let i = 0; i < 3; i++) {
     boxes[i] = new Box(450, 300 - i * 75, 50, 75);
   }
-  bird = new Bird(50, 300, 25);
+  bird = new Bird(150, 300, 25);
 
-  slingshot = new SlingShot(50, 300, bird.body);
+  slingshot = new SlingShot(150, 300, bird.body);
 
   const mouse = Mouse.create(canvas.elt);
   const options = {
@@ -25,6 +25,13 @@ function setup() {
   }
   mConstraint = MouseConstraint.create(engine, options);
   World.add(world, mConstraint);
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    slingshot.fly()
+  }
+
 }
 
 function draw() {
--- MatterExploration/MatterJsExp3/slingshot.js
+++ MatterExploration/MatterJsExp3/slingshot.js
@@ -8,18 +8,23 @@ class SlingShot {
       },
       bodyB: body,
       stiffness: 0.5,
-      length: 20
+      length: 40
     }
     this.sling = Constraint.create(options);
     World.add(world, this.sling);
+  }
 
+  fly() {
+    this.sling.bodyB = null;
   }
 
   show() {
-    stroke(255);
-    const posA = this.sling.pointA;
-    const posB = this.sling.bodyB.position;
-    line(posA.x, posA.y, posB.x, posB.y);
+    if (this.sling.bodyB) {
+      stroke(255);
+      const posA = this.sling.pointA;
+      const posB = this.sling.bodyB.position;
+      line(posA.x, posA.y, posB.x, posB.y);
+    }
   }
 
 }
